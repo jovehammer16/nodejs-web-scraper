7f904119156bd76795342abba488268d483982f0
Jove Tsui <siu@beacon.co.uk>
2023-08-20 00:36:38 +0100

    added test case, refactor a bit

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
+const { scrapeBySelector } = require('./scrape');
 
 // URL of the website you want to scrape
 const url = 'https://finance.yahoo.com';
 
-// Perform an HTTP GET request to fetch the HTML content
-axios.get(url)
-  .then(response => {
-    const html = response.data;
-    const $ = cheerio.load(html); // Load the HTML into cheerio
+// CSS selector to extract data
+const selector = '#marketsummary-itm-0 > h3 > fin-streamer';
 
-    // Use cheerio selectors to extract data
-    const indexSP500 = $('#marketsummary-itm-0 > h3 > fin-streamer').text();
-
-    console.log('S&P500: ', indexSP500);
+// Call the scrapeBySelector function and log the result
+scrapeBySelector(url, selector)
+  .then(extractedData => {
+    console.log('Extracted Data:', extractedData);
   })
   .catch(error => {
     console.error('Error:', error);
   });
-
 
